9c23fc460372187c97063f5016309eda02a3f9a9
nitish singh <nitishsingh925@gmail.com>
2024-04-06 14:21:10 +0530

    google auth add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 const Header = () => {
+  const { data: session } = useSession();
   return (
     <header className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto ">
@@ -25,7 +28,23 @@ const Header = () => {
           placeholder="Search"
           className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px] focus:outline-none"
         />
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <Image
+            src={session.user.image}
+            alt={session.user.name}
+            width={40}
+            height={40}
+            className="h-10 w-10 rounded-full cursor-pointer"
+            onClick={signOut}
+          />
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </header>
   );
